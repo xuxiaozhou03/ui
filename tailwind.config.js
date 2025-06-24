efaf4fda87a60b87586d23844ef8ed36e81c2e09
许晓周 <759811542@qq.com>
2025-06-24 13:39:33 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: ["./public/index.html", "./src/**/*.{html,js,ts,jsx,tsx,vue}"],
+  // 其他配置项
+  // ...
+  corePlugins: {
+    // 小程序不需要 preflight，因为这主要是给 h5 的，如果你要同时开发小程序和 h5 端，你应该使用环境变量来控制它
+    preflight: false,
+  },
+};
