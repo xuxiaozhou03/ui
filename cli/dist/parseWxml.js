895d7feb29cf75c935b6e276d85517087b996384
许晓周 <759811542@qq.com>
2025-07-22 16:59:11 +0800

    11

--- cli/dist/parseWxml.js
+++ cli/dist/parseWxml.js
@@ -2,12 +2,85 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseWxml = void 0;
 const htmlparser2_1 = require("htmlparser2");
+function splitClassNames(str) {
+    const dynamicMatches = [...str.matchAll(/\{\{[^}]+\}\}/g)];
+    // 用占位符替换动态表达式
+    let replaced = str;
+    dynamicMatches.forEach((m, i) => {
+        replaced = replaced.replace(m[0], `__DYNAMIC_${i}__`);
+    });
+    // 按空格分割
+    const parts = replaced.split(/\s+/).filter(Boolean);
+    // 恢复动态表达式
+    return parts.map((p) => {
+        if (p.startsWith("__DYNAMIC_")) {
+            const idx = Number(p.match(/\d+/)[0]);
+            return dynamicMatches[idx][0];
+        }
+        return p;
+    });
+}
 const parseClass = (key, value) => {
+    // // 处理纯文本
+    // if (!value.includes("{{") && !value.includes("bem") && !value.includes("?")) {
+    //   let classes = value.split(" ").filter(Boolean);
+    //   let isVar = false;
+    //   classes = classes
+    //     .filter(Boolean)
+    //     .map((cls) => {
+    //       if (cls.includes("-class")) {
+    //         isVar = true;
+    //         const newCls = cls.split("-").reduce((acc, part, index) => {
+    //           if (index === 0) {
+    //             return acc + part;
+    //           }
+    //           return acc + part.charAt(0).toUpperCase() + part.slice(1);
+    //         }, "");
+    //         return newCls;
+    //       }
+    //       return cls;
+    //     })
+    //     .sort((a, b) => {
+    //       if (a.includes("Class")) {
+    //         return 1;
+    //       }
+    //       if (b.includes("Class")) {
+    //         return -1;
+    //       }
+    //       return 1;
+    //     });
+    //   if (isVar) {
+    //     key = `:${key}`;
+    //     value = classes
+    //       .map((cls) => {
+    //         if (cls.includes("Class")) {
+    //           return cls.replace("Class", "class");
+    //         }
+    //         return `'${cls}'`;
+    //       })
+    //       .join(", ");
+    //     value = `cn(${value})`;
+    //   } else {
+    //     value = classes.join(" ");
+    //   }
+    //   return { [key]: value };
+    // }
+    // if (value.includes("{{") && !value.includes("utils")) {
+    //   const data = splitClassNames(value);
+    //   console.log(data, value);
+    // }
+    console.log("parseClass", splitClassNames(value));
     return {
         [key]: value,
     };
 };
 const parseAttrs = (attrs, isCustomElement) => {
+    if (isCustomElement) {
+        if (attrs["custom-class"] && attrs.class) {
+            attrs["custom-class"] = `${attrs.class} ${attrs["custom-class"]}`;
+            delete attrs.class;
+        }
+    }
     const newAttrs = Object.entries(attrs).reduce((acc, [key, value]) => {
         const map = {
             "wx:for": "v-for",
@@ -48,6 +121,11 @@ const parseAttrs = (attrs, isCustomElement) => {
                     key = `:${key}`;
                 }
             }
+        }
+        if (key.includes("class")) {
+            const obj = parseClass(key, value);
+            acc = Object.assign(Object.assign({}, acc), obj);
+            return acc;
         }
         acc[key] = value;
         return acc;
